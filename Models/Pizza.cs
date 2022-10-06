8b252e655534ab886bc36aa73fbb941c6d04ee79
Jeferson Santos <jeferson.santos@onfriday.com.br>
2022-10-06 09:51:48 -0300

    first commit

---
+++ Models/Pizza.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PizzaStore.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+    class PizzaDb : DbContext
+    {
+        public PizzaDb(DbContextOptions options) : base(options) { }
+        public DbSet<Pizza> Pizzas { get; set; } = null!;
+    }
+}
